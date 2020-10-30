2386ce8f2340af52ec6b6f59c45dd828b147a208
matutezam <58740758+matutezam@users.noreply.github.com>
2020-10-30 19:31:23 +0100

    Actividad 3

--- app/src/main/java/net/jaumebalmes/mzamorano/buscanumprimo/MainActivity.java
+++ app/src/main/java/net/jaumebalmes/mzamorano/buscanumprimo/MainActivity.java
@@ -5,30 +5,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static net.jaumebalmes.mzamorano.buscanumprimo.R.string.invalid_value;
+import static net.jaumebalmes.mzamorano.buscanumprimo.R.string.t_invalid_value;
+import static net.jaumebalmes.mzamorano.buscanumprimo.R.string.t_final_msg;
 
 public class MainActivity extends AppCompatActivity {
-
+                                                                //Definimos todas las variables
     private EditText editPrimo;
     private Button buttonCalc;
     private TextView textResult;
     private Primos primos = new Primos();
     private String cadBusca;
     private int numBusca;
+    private String primoCad;
 
     public void comienzaBusqueda(View v) {
-        cadBusca = String.valueOf(editPrimo.getText());
-        if (cadBusca.isEmpty()) {
-            textResult.setText(invalid_value);
-        } else if (cadBusca.length() > 4) {
-            textResult.setText(invalid_value);
+        cadBusca = String.valueOf(editPrimo.getText());         //Extraemos el texto para comprobar
+        if (cadBusca.isEmpty()) {                               //si el campo está vacío
+            textResult.setText("");
+            Toast.makeText(this, t_invalid_value, Toast.LENGTH_SHORT).show();
         } else {
-            numBusca = Integer.parseInt(cadBusca);
-            String primoCad = String.valueOf(primos.consultaPrimo(numBusca));
-            textResult.setText(primoCad);
+            numBusca = Integer.parseInt(cadBusca);              //Convertimos cadena en entero para
+            if(numBusca != 0) {                                 //poder trabajar con él
+                primoCad = String.valueOf(primos.consultaPrimo(numBusca));
+                textResult.setText(primoCad);
+                Toast.makeText(this, t_final_msg, Toast.LENGTH_SHORT).show();
+            } else {                                            //Mostramos mensaje si el valor es 0
+                textResult.setText("");
+                Toast.makeText(this, t_invalid_value, Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -36,8 +44,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editPrimo = findViewById(R.id.editPrimo);
+                                                                //Vinculamos nuestras variables con
+        editPrimo = findViewById(R.id.editPrimo);               //los Views por su ID
         buttonCalc = findViewById(R.id.buttonCalc);
         textResult = findViewById(R.id.textResult);
     }
