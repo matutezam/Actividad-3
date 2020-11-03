516aa86ba079d2fdbe5d3811cada5a530fa4dfbc
matutezam <58740758+matutezam@users.noreply.github.com>
2020-11-03 01:18:15 +0100

    Actividad 3

--- app/src/main/java/net/jaumebalmes/mzamorano/buscanumprimo/MainActivity.java
+++ app/src/main/java/net/jaumebalmes/mzamorano/buscanumprimo/MainActivity.java
@@ -9,33 +9,33 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static net.jaumebalmes.mzamorano.buscanumprimo.R.string.t_invalid_value;
-import static net.jaumebalmes.mzamorano.buscanumprimo.R.string.t_final_msg;
+import static net.jaumebalmes.mzamorano.buscanumprimo.R.string.toast_invalid_value;
+import static net.jaumebalmes.mzamorano.buscanumprimo.R.string.toast_final_message;
 
 public class MainActivity extends AppCompatActivity {
                                                                       //Definimos todas las variables
-    private EditText edit_prime;
-    private Button button_calc;
-    private TextView text_result;
+    private EditText editPrime;
+    private Button buttonCalc;
+    private TextView textResult;
     private Primes primes = new Primes();
-    private String find_string;
-    private int find_number;
-    private String prime_string;
+    private String findString;
+    private int findNumber;
+    private String primeString;
 
     public void findPrime(View v) {
-        find_string = String.valueOf(edit_prime.getText());           //Extraemos el texto para comprobar
-        if (find_string.isEmpty()) {                                  //si el campo está vacío
-            text_result.setText("");
-            Toast.makeText(this, t_invalid_value, Toast.LENGTH_SHORT).show();
+        findString = String.valueOf(editPrime.getText());             //Extraemos el texto para comprobar
+        if (findString.isEmpty()) {                                   //si el campo está vacío
+            textResult.setText("");
+            Toast.makeText(this, toast_invalid_value, Toast.LENGTH_SHORT).show();
         } else {
-            find_number = Integer.parseInt(find_string);              //Convertimos cadena en entero para
-            if(find_number != 0) {                                    //poder trabajar con él
-                prime_string = String.valueOf(primes.getPrime(find_number));
-                text_result.setText(prime_string);
-                Toast.makeText(this, t_final_msg, Toast.LENGTH_SHORT).show();
+            findNumber = Integer.parseInt(findString);                //Convertimos cadena en entero para
+            if(findNumber != 0) {                                     //poder trabajar con él
+                primeString = String.valueOf(primes.getPrime(findNumber));
+                textResult.setText(primeString);
+                Toast.makeText(this, toast_final_message, Toast.LENGTH_SHORT).show();
             } else {                                                  //Mostramos un mensaje si el valor es 0
-                text_result.setText("");
-                Toast.makeText(this, t_invalid_value, Toast.LENGTH_SHORT).show();
+                textResult.setText("");
+                Toast.makeText(this, toast_invalid_value, Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -45,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
                                                                 //Vinculamos nuestras variables con
-        edit_prime = findViewById(R.id.edit_prime);             //los Views por su ID
-        button_calc = findViewById(R.id.button_calc);
-        text_result = findViewById(R.id.text_result);
+        editPrime = findViewById(R.id.edit_prime);              //los Views por su ID
+        buttonCalc = findViewById(R.id.button_calc);
+        textResult = findViewById(R.id.text_result);
     }
 }
--- app/src/main/java/net/jaumebalmes/mzamorano/buscanumprimo/Primes.java
+++ app/src/main/java/net/jaumebalmes/mzamorano/buscanumprimo/Primes.java
@@ -7,76 +7,76 @@ import java.util.Arrays;
  */
 public class Primes {
 
-    private int[] primes_array;
-    private boolean array_ini = false;
+    private int[] primesArray;
+    private boolean arrayIni = false;
 
-    protected int[] ini(int prime_pos) {   //Este método se ejecuta una única vez para crear un vector que luego tomaremos como referencia
+    protected int[] ini(int primePos) {   //Este método se ejecuta una única vez para crear un vector que luego tomaremos como referencia
 
-        if (prime_pos == 1) {
-            primes_array = new int[prime_pos + 1];        //Para que no desborde el vector cuando definimos los primos iniciales
+        if (primePos == 1) {
+            primesArray = new int[primePos + 1];         //Para que no desborde el vector cuando definimos los primos iniciales
         } else {
-            primes_array = new int[prime_pos];
+            primesArray = new int[primePos];
         }
 
-        primes_array[0] = 2;                              //Definimos los primeros números primos del vector
-        primes_array[1] = 3;
-        int array_pos = 2;
+        primesArray[0] = 2;                              //Definimos los primeros números primos del vector
+        primesArray[1] = 3;
+        int arrayPos = 2;
 
-        for (int i = 5; array_pos < prime_pos; i += 2) {  //Saltamos los números pares
-            int div_count = 0;
+        for (int i = 5; arrayPos < primePos; i += 2) {   //Saltamos los números pares
+            int divCount = 0;
 
-            for (int j = 0; primes_array[j] * primes_array[j] <= i; j++) {  //Mientras no rebace la raiz (multiplica factores)
-                if (i % primes_array[j] == 0) {
-                    div_count++;
-                    break;                                                  //Rompe bucle cuando encuentra un divisor
+            for (int j = 0; primesArray[j] * primesArray[j] <= i; j++) {  //Mientras no rebace la raiz (multiplica factores)
+                if (i % primesArray[j] == 0) {
+                    divCount++;
+                    break;                                                //Rompe bucle cuando encuentra un divisor
                 }
             }
-            if (div_count < 1) {                                            //Comprueba el contador que sólo tenga dos divisores
-                primes_array[array_pos] = i;
-                array_pos++;
+            if (divCount < 1) {                                           //Comprueba el contador que sólo tenga dos divisores
+                primesArray[arrayPos] = i;
+                arrayPos++;
             }
         }
-        return primes_array;
+        return primesArray;
     }
 
-    public int getPrime(int prime_pos) {             //Método principal
-        if (prime_pos < 1) {
+    public int getPrime(int primePos) {              //Método principal
+        if (primePos < 1) {
             return 0;                                //Evitamos que falle retornando 0.
         }
-        if (!array_ini) {
-            if (prime_pos == 1) {                    //Creamos vector de dos posiciones para evitar que falle la función
-                ini(prime_pos + 1);        //ini pues necesita establecer dos números primos básicos para
-                array_ini = true;                    //poder calcular el resto del vector
+        if (!arrayIni) {
+            if (primePos == 1) {                    //Creamos vector de dos posiciones para evitar que falle la función
+                ini(primePos + 1);         //ini pues necesita establecer dos números primos básicos para
+                arrayIni = true;                    //poder calcular el resto del vector
             } else {
-                ini(prime_pos);
-                array_ini = true;
+                ini(primePos);
+                arrayIni = true;
             }
         }
-        if (prime_pos <= primes_array.length) {     //Si la posición del número primo ya ha sido calculada, devolvemos
-            return primes_array[prime_pos - 1];     //dicho número por pantalla
+        if (primePos <= primesArray.length) {       //Si la posición del número primo ya ha sido calculada, devolvemos
+            return primesArray[primePos - 1];       //dicho número por pantalla
         } else {
-            update(prime_pos);                       //Si la búsqueda rebasa el vector, reutilizamos la información y
-            return primes_array[prime_pos - 1];      //actualizamos sus datos a partir del último primo encontrado
+            update(primePos);                       //Si la búsqueda rebasa el vector, reutilizamos la información y
+            return primesArray[primePos - 1];       //actualizamos sus datos a partir del último primo encontrado
         }
     }
 
-    protected void update(int prime_pos) {           //Si el usuario pide un primo que no hayamos calculado
-        int last_prime = primes_array[primes_array.length - 1] + 2;    //Copiamos el último primo obtenido y sumamos dos para evitar duplicación
-        int array_pos = primes_array.length;                           //Especificamos posición donde guardar los nuevos primos encontrados
-        primes_array = Arrays.copyOf(primes_array, prime_pos);         //Sobreescribimos el vector agregando una nueva longitud
+    protected void update(int primePos) {           //Si el usuario pide un primo que no hayamos calculado
+        int lastPrime = primesArray[primesArray.length - 1] + 2;    //Copiamos el último primo obtenido y sumamos dos para evitar duplicación
+        int arrayPos = primesArray.length;                          //Especificamos posición donde guardar los nuevos primos encontrados
+        primesArray = Arrays.copyOf(primesArray, primePos);         //Sobreescribimos el vector agregando una nueva longitud
 
-        for (int i = last_prime; array_pos < prime_pos; i += 2) {      //Saltamos los números pares
-            int div_count = 0;
+        for (int i = lastPrime; arrayPos < primePos; i += 2) {      //Saltamos los números pares
+            int divCount = 0;
 
-            for (int j = 0; primes_array[j] * primes_array[j] <= i; j++) {  //Mientras no rebace la raiz (multiplica factores)
-                if (i % primes_array[j] == 0) {
-                    div_count++;
+            for (int j = 0; primesArray[j] * primesArray[j] <= i; j++) {    //Mientras no rebace la raiz (multiplica factores)
+                if (i % primesArray[j] == 0) {
+                    divCount++;
                     break;                                                  //Rompe bucle cuando encuentra un divisor
                 }
             }
-            if (div_count < 1) {                                            //Comprueba que sólo tenga dos divisores
-                primes_array[array_pos] = i;
-                array_pos++;
+            if (divCount < 1) {                                             //Comprueba que sólo tenga dos divisores
+                primesArray[arrayPos] = i;
+                arrayPos++;
             }
         }
     }
